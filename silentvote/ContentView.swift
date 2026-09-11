af2ab387da121c0df72a872d101ad552b63d6b5e
sheltonsuen <sheltonsuen@gmail.com>
2026-09-11 16:12:49 +0800

    Initial Commit

---
+++ silentvote/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  silentvote
+//
+//  Created by shelton on 2026/9/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ silentvote/silentvoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  silentvoteApp.swift
+//  silentvote
+//
+//  Created by shelton on 2026/9/11.
+//
+
+import SwiftUI
+
+@main
+struct silentvoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
